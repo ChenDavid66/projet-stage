4d60d187339a4931010378d9dd7d836b41ec7c4c
DAVID <vivadu14@hotmail.fr>
2020-01-29 14:58:42 +0100

    erreur

---
+++ services/passport.js
@@ -0,0 +1,38 @@
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const keys = require('../config/keys');
+const mongoose= require('mongoose');
+
+const User = mongoose.model('user');
+passport.serializeUser((user,done) => {
+  done(null,user.id);
+});
+passport.deserializeUser((id,done) => {
+  User.findByID(id).then(user => {
+    done(null,user);
+  });
+});
+
+passport.initialize()
+
+passport.use(
+  new GoogleStrategy(
+    {
+    clientID: keys.googleClientID,
+    clientSecret: keys.googleClientSecret,
+    callbackURL: '/auth/google/callback'
+    },
+    (accessToken, refreshToken, profile,done) => {
+      User.findOne({googleID: profile.id})
+        .then((existinguser) => {
+          if (existinguser) {
+            done(null,existinguser);
+          } else {
+            new User ({googleID: profile.id})
+              .save()
+              .then(user => done(null,user))
+          }
+        });
+    }
+  )
+);
