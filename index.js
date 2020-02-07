55a5805692a3a05bcfb0d751c87513c8c12cdc77
DAVID <vivadu14@hotmail.fr>
2020-02-07 10:32:35 +0100

    final

--- index.js
+++ index.js
@@ -4,10 +4,11 @@ const cookieSession = require('cookie-session');
 const passport = require('passport');
 const bodyParser = require('body-parser');
 const keys = require('./config/keys');
-require('./models/user');
+require('./models/User');
 require('./models/Surveys');
 require('./services/passport');
 
+mongoose.Promise = global.Promise;
 mongoose.connect(keys.mongoURI);
 
 const app = express();
@@ -15,23 +16,25 @@ const app = express();
 app.use(bodyParser.json());
 app.use(
   cookieSession({
-    maxAge:30 * 24 * 60 * 60 * 1000,
-    keys:[keys.cookieKey]
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    keys: [keys.cookieKey]
   })
 );
 app.use(passport.initialize());
 app.use(passport.session());
 
-require('./routes/authRoutes.js')(app) ;
-require('./routes/billingRoutes.js')(app) ;
+require('./routes/authRoutes')(app);
+require('./routes/billingRoutes')(app);
 require('./routes/surveyRoutes')(app);
 
-if (process.env.NODE_ENV === 'production'){
+if (process.env.NODE_ENV === 'production') {
+
   app.use(express.static('client/build'));
 
-  const path = require ('path');
-  app.get('*',(req,res) => {
-    res.sendFile(path.resolve(__dirname,'client','build','index.html'));
+
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   });
 }
 
